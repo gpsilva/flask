b046b555c66336244b917dee1b2ce005f5fb4e31
gilvan <gilvanps@ciandt.com>
2018-08-27 14:11:53 -0300

    updated .gitignore andtables.py

--- app/models/tables.py
+++ app/models/tables.py
@@ -1,2 +1,45 @@
 from app import db
 
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True)
+    password = db.Column(db.String,)
+    name = db.Column(db.String)
+    email = db.Column(db.String, unique=True)
+
+    def __init__(self, username, password, name, email):
+        self.username = username
+        self.password = password
+        self.name = name
+        self.email = email
+
+    def __repr__(self):
+        return "<User %r>" % self.username
+
+class Post(db.Model):
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text)
+    id_user = db.Column(db.Integer, db.ForeingKey('users.id'))
+
+    user = db.relationship('User', ForeingKey=user_id)
+
+    def __init__(self, content, user_id):
+        self.content = content
+        self.user_id = user_id
+
+    def __repr__(self):
+        return "<Post %r>" self.id
+
+class Follow(db.Model):
+    __tablename__ = "follow"
+
+    id = db.Column(db.Integer, primary_key=True)
+    id_user = db.Column(db.Integer, db.ForeingKey('users.id'))
+
+    user = db.relationship('User', ForeingKeys=user_id)
+    follower = db.relationship('User', ForeingKeys=follower_id)
+    
