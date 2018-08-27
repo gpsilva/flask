2d2befc314c1712c2ba6715859e33ccc1c7f02cf
gilvan <gilvanps@ciandt.com>
2018-08-27 17:00:48 -0300

    finished html before bootstrap

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,15 +1,7 @@
 from flask import render_template
 from app import app
 
-@app.route("/index")
-@app.route("/")
-def index():
-    return render_template('index.html')
-
-# @app.route("/test", defautls={'name': None})
-#@app.route("/test/<name")
-#def test(name):
-#   if name:
-#        return "Hello, $s!" % name
-#    else:
-#        return "Hello, user!"
+@app.route("/index/<user>")
+@app.route("/", defaults={"user":None})
+def index(user):
+    return render_template('index.html', user=user)
