49dbe0cef35b231c03f2dfd4fe1ee4c0c344c707
gilvan <gilvanps@ciandt.com>
2018-08-27 16:08:05 -0300

    updated some new routes

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,6 +1,14 @@
 from app import app
 
-
+@app.route("/index")
 @app.route("/")
 def index():
     return "Hello World!"
+
+@app.route("/test", defautls={'name': None})
+@app.route("/test/<name")
+def test(name):
+    if name:
+        return "Hello, $s!" % name
+    else:
+        return "Hello, user!"
