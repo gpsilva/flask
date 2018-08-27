8cef199a86272fdaced641a505db8828708dac2d
gilvan <gilvanps@ciandt.com>
2018-08-27 16:40:09 -0300

    added new template with html

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,14 +1,15 @@
+from flask import render_template
 from app import app
 
 @app.route("/index")
 @app.route("/")
 def index():
-    return "Hello World!"
+    return render_template('index.html')
 
-@app.route("/test", defautls={'name': None})
-@app.route("/test/<name")
-def test(name):
-    if name:
-        return "Hello, $s!" % name
-    else:
-        return "Hello, user!"
+# @app.route("/test", defautls={'name': None})
+#@app.route("/test/<name")
+#def test(name):
+#   if name:
+#        return "Hello, $s!" % name
+#    else:
+#        return "Hello, user!"
